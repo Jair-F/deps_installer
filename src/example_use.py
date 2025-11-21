3e714f301e6e79c59aa93b24a26047a09f5e68c6
Jair <jair.fehlauer@gmail.com>
2025-11-21 19:40:53 +0200

    renamed example file

--- src/example_use.py
+++ src/example_use.py
@@ -1,13 +1,16 @@
 import argparse
+import sys
+
+import pyuac
 import deps_installer
 
 RUN_AS_ADMIN = False
 
 if __name__ == "__main__":
-    # if RUN_AS_ADMIN and not pyuac.isUserAdmin():
-    #     print("Re-launching as admin!")
-    #     pyuac.runAsAdmin()
-    #     sys.exit(0)
+    if RUN_AS_ADMIN and not pyuac.isUserAdmin():
+        print("Re-launching as admin!")
+        pyuac.runAsAdmin()
+        sys.exit(0)
 
     parser = argparse.ArgumentParser(description="A sample program using argparse.")
     parser.add_argument("--install_deps", action="store_true", help="Enable verbose output.")
